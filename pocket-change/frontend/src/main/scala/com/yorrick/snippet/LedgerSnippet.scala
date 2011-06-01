67fc752c978bdea9903274033f6da9e2f94cf461
unknown <yjansen@.cpage.fr>
2011-06-01 17:01:10 +0200

    Test des formulaires

--- pocket-change/frontend/src/main/scala/com/yorrick/snippet/LedgerSnippet.scala
+++ pocket-change/frontend/src/main/scala/com/yorrick/snippet/LedgerSnippet.scala
@@ -8,6 +8,7 @@ import xml.{Text, NodeSeq}
 import model.{Task, TaskImportance}
 import net.liftweb.util.BindHelpers._
 import net.liftweb.http.{S, RequestVar, SHtml, DispatchSnippet}
+import net.liftweb.common.Full
 
 object amount extends RequestVar("0")
 object description extends RequestVar("")
@@ -16,6 +17,8 @@ object LedgerSnippet extends DispatchSnippet {
 
   def dispatch : DispatchIt = {
     case "add" => addLedger _
+    case "listFormElements" => listFormElements _
+    case "editLink" => editLink _
   }
 
   private def addLedger(content : NodeSeq) : NodeSeq = {
@@ -28,6 +31,7 @@ object LedgerSnippet extends DispatchSnippet {
         } else{
           // redirect
           println("processing entry add with value " + amount + ", " + description)
+          S.redirectTo("/")
         }
       } catch {
         case e : NumberFormatException => S.error("Wrong value")
@@ -41,6 +45,31 @@ object LedgerSnippet extends DispatchSnippet {
     )
   }
 
+  private def listFormElements(content : NodeSeq) : NodeSeq = {
+    def checkboxCallback(checked : Boolean) = {
+      println("checkboxCallback " + checked)
+    }
+
+    bind("entry", content,
+      "checkbox" -> SHtml.checkbox_id(false, checkboxCallback _, Full("snazzy"), "class" -> "woohoo"),
+      "submit"      -> SHtml.submit("Send", () => ())
+    )
+  }
+
+  private def editLink(content : NodeSeq) : NodeSeq = {
+    val redirectPath = "/"
+    val linkMarkupId = "editLink"
+
+    def linkCallback = {
+      S.redirectTo(redirectPath)
+    }
+
+    (
+      // TODO changer le lien, et mettre un lien vers une page d'edition
+      ("#"+ linkMarkupId)    #> SHtml.link(redirectPath, linkCallback _, content \\ "a" \ ("@" + linkMarkupId))
+    ).apply(content)
+  }
+
 
 //  private def addLedger(content : NodeSeq) : NodeSeq = {
 //    println("addLedger rendering " + Thread.currentThread.getName)
@@ -49,7 +78,16 @@ object LedgerSnippet extends DispatchSnippet {
 //    var amount = "0"
 //
 //    def processEntryAdd = {
-//      println("processing entry add with value " + amount + ", " + desc + "  " + Thread.currentThread.getName)
+//      try {
+//        if (amount.toDouble < 0) {
+//          S.error("Invalid amount")
+//        } else{
+//          // redirect
+//          println("processing entry add with value " + amount + ", " + description)
+//        }
+//      } catch {
+//        case e : NumberFormatException => S.error("Wrong value")
+//      }
 //    }
 //
 //    bind("entry", content,
--- pocket-change/frontend/src/main/scala/com/yorrick/view/FormsTestView.scala
+++ pocket-change/frontend/src/main/scala/com/yorrick/view/FormsTestView.scala
@@ -8,6 +8,8 @@ object FormsTestView extends LiftView {
 
   def dispatch = {
     case "test1" => test1 _
+    case "test2" => test2 _
+    case "test3" => test3 _
     // redirection sur le premier test par defaut
     case _ => test1 _
   }
@@ -15,9 +17,34 @@ object FormsTestView extends LiftView {
   def test1 : NodeSeq =
     <lift:surround with="default" at="content">
       <lift:Ledger.add form="POST">
-        <entry:description /> <entry:amount /><br />
+        <entry:description />
+        <entry:amount /><br />
         <entry:submit />
       </lift:Ledger.add>
     </lift:surround>
+
+  def test2 : NodeSeq =
+    <lift:surround with="default" at="content">
+      <lift:Ledger.listFormElements form="POST">
+        <entry:checkbox/>
+        <entry:submit />
+      </lift:Ledger.listFormElements>
+    </lift:surround>
+
+  def test3 : NodeSeq =
+    <lift:surround with="default" at="content">
+      <h2>Liste des taches</h2>
+      <ul >
+        <div class="lift:Tasks.listEditorFriendly">
+          <li>
+            <h3 id="label">Label de la tâche</h3>
+            <p class="description">Description : </p>
+            <div class="lift:Ledger.editLink">
+              <a id="editLink">Editer la tâche</a>
+            </div>
+          </li>
+        </div>
+      </ul>
+     </lift:surround>
 
 }
