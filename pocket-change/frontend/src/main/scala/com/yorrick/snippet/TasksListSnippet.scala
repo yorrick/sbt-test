67e8a07dd9eec96f71702cfd93f93780ee10babe
unknown <yjansen@.cpage.fr>
2011-06-14 16:16:55 +0200

    Séparation du snippet d'édition et de visualisation

---
+++ pocket-change/frontend/src/main/scala/com/yorrick/snippet/TasksListSnippet.scala
@@ -0,0 +1,41 @@
+package com.yorrick.snippet
+
+import com.yorrick.model.Task
+import requestvars.{currentTask, taskImportance}
+import net.liftweb.http.{SHtml, DispatchSnippet}
+import xml.NodeSeq._
+import xml.{Attribute, Null, NodeSeq, Text}
+import net.liftweb.common.Full
+import net.liftweb.util.BindHelpers._
+
+object TasksListSnippet extends DispatchSnippet {
+
+  def dispatch : DispatchIt = {
+    case "viewTask" => viewTask _
+  }
+
+   private def viewTask(content : NodeSeq) : NodeSeq = {
+    val result = Task.getTasks(taskImportance.is).flatMap(task => {
+        val redirectPath = "/tasks/edition/"
+
+        val imageTag = task.image match {
+          case Some(image) =>
+            val imageSource = "/tasks/image/" + task.id
+            <img id="image" alt="Image de test"/> % Attribute(None, "src", Text(imageSource), Null)
+          case None =>
+            <span>Pas d'image pour cette tâche</span>
+        }
+
+        (
+          "#label *"        #> (task.label + "(" + task.id + ", " + task.importance.toString + ")") &
+          ".description *+" #> task.detail &
+          "#image"          #> imageTag &
+          "#editLink"       #> SHtml.link(redirectPath, () => currentTask(Full(task)), content \\ "a")
+        ).apply(content)
+      }
+    )
+
+    result
+  }
+
+}
--- pocket-change/frontend/src/main/scala/com/yorrick/snippet/TasksSnippet.scala
+++ pocket-change/frontend/src/main/scala/com/yorrick/snippet/TasksSnippet.scala
@@ -4,43 +4,16 @@ package com.yorrick {
 package snippet {
 
 import net.liftweb.util.BindHelpers._
-import requestvars.{currentTask, taskImportance}
-import xml.NodeSeq._
+import requestvars.currentTask
 import net.liftweb.common.{Empty, Full, Box}
-import java.awt.Color
 import net.liftweb.http._
 import model.{Image, Task, TaskImportance}
-import xml.{Null, Attribute, Text, NodeSeq}
+import xml.NodeSeq
 
-class TasksSnippet extends StatefulSnippet {
+class TasksEditionSnippet extends StatefulSnippet {
 
   def dispatch : DispatchIt = {
-    case "viewTask" => viewTask _
     case "editTask" => editTask _
-  }
-
-  private def viewTask(content : NodeSeq) : NodeSeq = {
-    val result = Task.getTasks(taskImportance.is).flatMap(task => {
-        val redirectPath = "/tasks/edition/"
-
-        val imageTag = task.image match {
-          case Some(image) =>
-            val imageSource = "/tasks/image/" + task.id
-            <img id="image" alt="Image de test"/> % Attribute(None, "src", Text(imageSource), Null)
-          case None =>
-            <span>Pas d'image pour cette tâche</span>
-        }
-
-        (
-          "#label *"        #> (task.label + "(" + task.id + ", " + task.importance.toString + ")") &
-          ".description *+" #> task.detail &
-          "#image"          #> imageTag &
-          "#editLink"       #> SHtml.link(redirectPath, () => currentTask(Full(task)), content \\ "a")
-        ).apply(content)
-      }
-    )
-
-    result
   }
 
   var alreadyModifyied = false
